e66af348079f4afbed6c5511dac7fd3dcc61415a
williamtwobit <william@twobit.cc>
2017-08-03 14:22:10 -0400

    implemented quicksort

--- app.js
+++ app.js
@@ -67,3 +67,29 @@ function mergeSort(arr) {
 }
 
 // mergeSort(arr);
+
+
+function quickSort(array, start=0, end=array.length){
+  start = start;
+  end = end;
+  if(start >= end){
+    return array;
+  }
+  const middle = partition(array,start,end);
+  array = quickSort(array,start,middle);
+  array = quickSort(array,middle+1,end);
+  return array;
+}
+
+function partition(array,start,end){
+  const pivot = array[end-1];
+  let j = start;
+  for(let i=start; i<end-1; i++){
+    if(array[i] <= pivot){
+      swap(array,i,j);
+      j++;
+    }
+  }
+  swap(array,end-1,j);
+  return j;
+}
