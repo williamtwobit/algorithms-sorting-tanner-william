94aa3dbf0e9082df5a6f92aa7d44f38304665bca
tannaurus <contacttannergill@gmail.com>
2017-08-03 08:58:47 -0700

    merge & mergeSort function completed.

--- app.js
+++ app.js
@@ -27,3 +27,41 @@ function bubbleSort(array){
 const arr = [15,4,8,16,42,23];
 
 // bubbleSort(arr);
+
+function merge(left, right, arr){
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let outputIndex = 0;
+
+  while(leftIndex < left.length && rightIndex < right.length){
+    if(left[leftIndex] < right[rightIndex]){
+      arr[outputIndex++] = right[rightIndex++];
+    }else{
+      arr[outputIndex++] = right[rightIndex++];
+    }
+  }
+
+  for(let i = leftIndex; i < left.length; i++){
+    arr[outputIndex++] = left[i];
+  }
+
+  for(let i = rightIndex; i < right.length; i++){
+    arr[outputIndex++] = right[i];
+  }
+  
+  return arr;
+}
+
+function mergeSort(arr) {
+  if(arr.length <= 1){
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2);
+  let left = arr.slice(0, middle);
+  let right = arr.slice(middle, arr.length);
+
+  left = mergeSort(left);
+  right = mergeSort(right);
+  return merge(left, right, arr);
+}
